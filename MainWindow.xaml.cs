122b3244812626c588a4294de4cad000bc981490
Aravinthan <134296054+Aravinthan-A@users.noreply.github.com>
2023-07-05 19:26:22 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Increment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int increment = Convert.ToInt32(txtvalue.Text);
+            int length = Convert.ToInt32(txtend.Text);
+            int start = Convert.ToInt32(txthigh.Text);
+
+            for (int i= start; i < length; i=i+ increment)
+            {
+                MessageBox.Show(i.ToString());
+            }
+        }
+    }
+}
